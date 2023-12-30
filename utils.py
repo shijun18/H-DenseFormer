c1efb3ed7133428e73e787c11787d7e50c51dde4
Jun Shi <shijun18@mail.ustc.edu.cn>
2023-12-30 20:14:23 +0800

    add tools for computing metrics

--- utils.py
+++ utils.py
@@ -2,7 +2,75 @@ import os
 import h5py
 import numpy as np
 import torch
+import SimpleITK as sitk
 
+
+def cal_score(predict,target):
+    overlap_measures_filter = sitk.LabelOverlapMeasuresImageFilter()
+
+    overlap_measures_filter.Execute(target, predict)
+    Jaccard = overlap_measures_filter.GetJaccardCoefficient()
+    Dice = overlap_measures_filter.GetDiceCoefficient()
+    VolumeSimilarity = overlap_measures_filter.GetVolumeSimilarity()
+    FalseNegativeError = overlap_measures_filter.GetFalseNegativeError()
+    FalsePositiveError = overlap_measures_filter.GetFalsePositiveError()
+    # print(Jaccard,Dice,VolumeSimilarity,FalseNegativeError,FalsePositiveError)
+    
+    hausdorff_distance_filter = sitk.HausdorffDistanceImageFilter()
+    
+    try:
+        hausdorff_distance_filter.Execute(target, predict)
+    except RuntimeError:
+        return Jaccard,Dice,VolumeSimilarity,FalseNegativeError,FalsePositiveError, 144, 144
+    HausdorffDistance = hausdorff_distance_filter.GetHausdorffDistance()
+    # Symmetric surface distance measures
+    segmented_distance_map = sitk.Abs(sitk.SignedMaurerDistanceMap(predict, squaredDistance=False, useImageSpacing=True))
+    segmented_surface = sitk.LabelContour(predict)
+
+    # Use the absolute values of the distance map to compute the surface distances (distance map sign, outside or inside 
+    # relationship, is irrelevant)
+    # label = 1
+    reference_distance_map = sitk.Abs(sitk.SignedMaurerDistanceMap(target, squaredDistance=False))
+    reference_surface = sitk.LabelContour(target)
+
+    statistics_image_filter = sitk.StatisticsImageFilter()
+    # Get the number of pixels in the reference surface by counting all pixels that are 1.
+    statistics_image_filter.Execute(reference_surface)
+    num_reference_surface_pixels = int(statistics_image_filter.GetSum()) 
+        
+    # Multiply the binary surface segmentations with the distance maps. The resulting distance
+    # maps contain non-zero values only on the surface (they can also contain zero on the surface)
+    seg2ref_distance_map = reference_distance_map*sitk.Cast(segmented_surface, sitk.sitkFloat32)
+    ref2seg_distance_map = segmented_distance_map*sitk.Cast(reference_surface, sitk.sitkFloat32)
+        
+    # Get the number of pixels in the reference surface by counting all pixels that are 1.
+    statistics_image_filter.Execute(segmented_surface)
+    num_segmented_surface_pixels = int(statistics_image_filter.GetSum())
+    
+    # Get all non-zero distances and then add zero distances if required.
+    seg2ref_distance_map_arr = sitk.GetArrayViewFromImage(seg2ref_distance_map)
+    seg2ref_distances = list(seg2ref_distance_map_arr[seg2ref_distance_map_arr!=0]) 
+    seg2ref_distances = seg2ref_distances + \
+                        list(np.zeros(num_segmented_surface_pixels - len(seg2ref_distances)))
+    ref2seg_distance_map_arr = sitk.GetArrayViewFromImage(ref2seg_distance_map)
+    ref2seg_distances = list(ref2seg_distance_map_arr[ref2seg_distance_map_arr!=0]) 
+    ref2seg_distances = ref2seg_distances + \
+                        list(np.zeros(num_reference_surface_pixels - len(ref2seg_distances)))
+        
+    all_surface_distances = seg2ref_distances + ref2seg_distances
+
+    # The maximum of the symmetric surface distances is the Hausdorff distance between the surfaces. In 
+    # general, it is not equal to the Hausdorff distance between all voxel/pixel points of the two 
+    # segmentations, though in our case it is. More on this below.
+    mean_surface_distance = np.mean(all_surface_distances)
+    median_surface_distance = np.median(all_surface_distances)
+    std_surface_distance = np.std(all_surface_distances)
+    max_surface_distance = np.max(all_surface_distances)
+    hd_95 = np.percentile(all_surface_distances,95)
+    # print(hd_95)
+    # print(HausdorffDistance)
+
+    return Jaccard,Dice,VolumeSimilarity,FalseNegativeError,FalsePositiveError, HausdorffDistance, hd_95
 
 def hdf5_reader(data_path, key):
     hdf5_file = h5py.File(data_path, 'r')
